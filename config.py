155c854981f6d0adc534bb6f1042321b3273ac25
COEP <vinitnehe100@gmail.com>
2026-02-19 02:58:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,21 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()  # Load environment variables from .env
+# Load .env only if running locally
+if os.environ.get("FLASK_ENV") != "production":
+    load_dotenv()  # This allows you to use .env locally
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
-    MONGO_URI = os.getenv("MONGO_URI", "your_mongodb_connection_string")
+    # Read environment variables
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    MONGO_URI = os.environ.get("MONGO_URI")
+
+    # Optional: add debug flag
+    DEBUG = os.environ.get("DEBUG", "False").lower() in ("true", "1", "yes")
+
+    # Ensure mandatory variables are set
+    if not SECRET_KEY:
+        raise Exception("SECRET_KEY is not set in environment variables!")
+    if not MONGO_URI:
+        raise Exception("MONGO_URI is not set in environment variables!")
+
